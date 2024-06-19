f46307fb5993be0506f4cba05350ffe77f79883b
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2024-06-18 22:46:43 -0300

    Inserção de validações na classe Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -15,10 +15,36 @@ public class Jogador {
     }
 
     public Carta jogada(Carta cartaNaMesa){
+        //PRIMEIRA MÃO
+    if (cartas.get(0).valor > cartaNaMesa.valor ){
         return cartas.get(0);
+
+    } else if (cartas.get(0).valor < cartaNaMesa.valor+1 && cartas.get(1).valor > cartas.get(3).valor) {
+        return cartas.get(1);
+
+    } else if (cartas.get(0).valor < cartas.get(3).valor && cartas.get(1).valor < cartas.get(3).valor) {
+        return cartas.get(2);
+
+        //SEGUNDA MÃO
+    } else if (cartas.get(1).valor == cartaNaMesa.valor+1) {
+        return cartas.get(1);
+
+    } else if (cartas.get(1).valor < cartaNaMesa.valor+1 && cartas.get(0).valor > cartas.get(4).valor) {
+        return cartas.get(0);
+
+    } else if (cartas.get(1).valor < cartaNaMesa.valor+1 && cartas.get(1).valor < cartas.get(4).valor && cartas.get(0).numero < cartas.get(4).valor) {
+        return cartas.get(2);
+
+        //TERCEIRA MÃO
+    }else if (cartas.get(2).valor == cartaNaMesa.valor+1) {
+        return cartas.get(2);
+
+    }else if (cartas.get(2).valor< cartaNaMesa.valor+1 && cartas.get(0).valor > cartas.get(5).valor){
+        return  cartas.get(0);
+    }else {
+        return cartas.get(1);
+        }
     }
-
-
 
     public void exibeCartas(){
         cartas.forEach(c -> {
